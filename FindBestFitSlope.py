20d52414ed16013c7118d30f2aa9b726df24f950
Onur Yücedağ <olmaditekrar@gmail.com>
2017-09-23 19:51:15 +0300

    R Squared Error

--- FindBestFitSlope.py
+++ FindBestFitSlope.py
@@ -12,10 +12,20 @@ def best_fit_slope_and_intercept(xs,ys) :
 			((mean(xs)*mean(xs)) - mean(xs*xs) ))
 	b = mean(ys) - m*mean(xs)
 	return m ,b 
+def squared_error(ys_orig,ys_line):
+	return sum((ys_line-ys_orig)**2)
+
+def coefficent_of_determination(ys_orig,ys_line):
+	y_mean_line =[mean(ys_orig)for y in ys_orig]
+	squared_error_regr = squared_error(ys_orig,ys_line)
+	squared_error_y_mean = squared_error(ys_orig,y_mean_line)
+	return 1- (squared_error_regr/squared_error_y_mean)
 
 m,b = best_fit_slope_and_intercept(xs,ys)
 regression_line =[(m*x)+b for x in xs] # Get every x in the xs array and get the y values from it while calculating the (mx+b) !
 #We can now predict the outputs from this equation ! We have a equation that we found out the m and b of it !
+r_squared = coefficent_of_determination(ys,regression_line)
+print(r_squared)
 plt.scatter(xs,ys)
 plt.plot(xs,regression_line)
 plt.show()
