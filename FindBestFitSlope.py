c5ef7d98039e06e32aab86665c3070a06c4089a5
Onur Yücedağ <olmaditekrar@gmail.com>
2017-09-25 01:26:54 +0300

    Testing the assumptions !

--- FindBestFitSlope.py
+++ FindBestFitSlope.py
@@ -2,10 +2,26 @@ from statistics import mean
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import style
+import random
+
+def create_dataset(hm,variance,step=2,correlation=False): # We create a dataset that we change and test it with correlations !
+	val = 1
+	ys = []
+	for i in range(hm):
+		y = val + random.randrange(-variance,variance)
+		ys.append(y)
+		if correlation and correlation =='pos':
+			val+=step
+		elif correlation and correlation == 'neg':
+			val-=step
+	xs = [i for i in range(len(ys))]
+
+	return np.array(xs,dtype=np.float64),np.array(ys,dtype=np.float64)
+
 style.use('fivethirtyeight')
 
-xs = np.array([1,2,3,4,5,6] , dtype = np.float64)
-ys = np.array([5,4,6,5,6,7], dtype = np.float64)
+# xs = np.array([1,2,3,4,5,6] , dtype = np.float64)
+# ys = np.array([5,4,6,5,6,7], dtype = np.float64)
 
 def best_fit_slope_and_intercept(xs,ys) :
 	m = (((mean(xs)*mean(ys)) - mean(xs*ys)) /
@@ -21,11 +37,17 @@ def coefficent_of_determination(ys_orig,ys_line):
 	squared_error_y_mean = squared_error(ys_orig,y_mean_line)
 	return 1- (squared_error_regr/squared_error_y_mean)
 
+xs,ys = create_dataset(40,40,2,correlation='pos')
 m,b = best_fit_slope_and_intercept(xs,ys)
+
+predict_x = 8
+predict_y = (m*predict_x)+b
+
 regression_line =[(m*x)+b for x in xs] # Get every x in the xs array and get the y values from it while calculating the (mx+b) !
 #We can now predict the outputs from this equation ! We have a equation that we found out the m and b of it !
 r_squared = coefficent_of_determination(ys,regression_line)
 print(r_squared)
 plt.scatter(xs,ys)
+plt.scatter(predict_x,predict_y , s=100)
 plt.plot(xs,regression_line)
 plt.show()
